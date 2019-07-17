714a2dfd58be4de0f3f9dc1e2e1e8b1bf9937379
Alan Krajina <alankrajina88@gmail.com>
2019-07-17 18:18:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,13 +14,13 @@ module FormsLab
 
   
   post '/pirates' do
-  @pirate = Pirate.new(params[:pirate])        ---> (all params are from FORM (attr.acces) == argument HASH for Pirate.new -> pirate info & nested ship ARRAY)
+  @pirate = Pirate.new(params[:pirate]) 
 
-  params[:pirate][:ships].each do |details|    ---> (three arguments iteration "name","type","booty")(nested SHIPS)
-    Ship.new(details)                          ---> creating new ship objects with 3 arguments
+  params[:pirate][:ships].each do |details|
+    Ship.new(details)                     
   end
  
-  @ships = Ship.all                            ---> saving ship objects to use in ERB
+  @ships = Ship.all                     
  
   erb :'pirates/show'
 end
